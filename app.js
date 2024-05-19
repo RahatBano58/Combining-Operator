0dfa6e28dfd85d9c4c91478b5e3c38686e054c63
Rahat Bano <160172836+RahatBano58@users.noreply.github.com>
2024-05-19 21:30:50 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+// Combining Operators
+//  Multiplication & Addition 
+let result = 3 + 4 * 5;
+console.log(result); //23
+//  Multiplication & Subtraction
+let result1 = 5 - 3 * 5;
+console.log(result1); // -10
+// Division & Multiplication 
+let result2 = 5 * 16 / 4;
+console.log(result2); // 20
+//Addition & Subtraction
+let result3 = 6 + 4 - 5;
+console.log(result3); // 5
+// Division , Multiplication & Addition
+let result4 = 144 / 4 + 5 * 2;
+console.log(result4);
+// Multiplication , Addition & Subtraction
+let result5 = 6 + 5 * 4 - 5;
+console.log(result5);
+// Division , Multiplication, Addition & Subtraction
+let result6 = 6 + 5 - 3 * 4 / 2;
+console.log(result6); //  5
+let result7 = 121 / 11 + 9 - 4 * 3;
+console.log(result7);
+export {};
